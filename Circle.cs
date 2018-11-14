8ba41594f18fb0919fab5e34ae1a3f4ef48be1d4
AntohaG98 <43339796+AntohaG98@users.noreply.github.com>
2018-11-14 12:20:57 +0300

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab2
+{
+    public class Circle : Figure
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public override double Area()
+        {
+            return Math.PI* Radius * Radius;
+        }
+        public override string ToString()
+        {
+            return $"Круг с радиусом {Radius} имеет площадь {Area()}";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Lab2
+{  class Program
+    {  static void PrintFigure(Figure figure) 
+           {  var num = figure.Area();
+            Console.WriteLine(num);
+            figure.Print(); }
+        static void Main(string[] args)
+        {
+            var rect = new Rectangle(12, 34);
+            var sqr = new Square(30);
+            var crl = new Circle(5.5);
+            rect.Print();
+            sqr.Print();
+            crl.Print();
+            Console.WriteLine();
+            PrintFigure(rect);
+            PrintFigure(sqr);
+            PrintFigure(crl);
+        }
+    }
+}
